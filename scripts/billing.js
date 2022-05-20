66f6eb98df790b2ec03d0e230c89eb693e51bc24
Koech-code <nixonkipkorir01@gmail.com>
2022-05-20 19:17:02 +0300

    create a separate folder for querying and writing functions

---
+++ scripts/billing.js
@@ -0,0 +1,37 @@
+// let addButton=document.querySelector("button");
+// let addTable=document.querySelector("table")
+
+// let descriptionInput=document.querySelector("#description");
+// let costInput=document.querySelector("#cost");
+// let quantityInput=document.querySelector("#quantity");
+// let vatInput=document.querySelector("#vat");
+// let totalsInput=document.querySelector("#totals");
+
+// addButton.addEventListener("click", () =>{
+//     let description=descriptionInput.value;
+//     let cost=cost.value;
+//     let quantity=quantityInput.value;
+//     let vat=vatInput.value;
+//     let totals=totalsInput.value;
+
+//     let template = `
+//     <tr>
+//         <td>${description}</td>
+//         <td>${cost}</td>
+//         <td>${quantity}</td>
+//         <td>${vat}</td>
+//         <td>${totals}</td>
+//         // <td>Actions</td>
+//     </tr>
+//     // <tr>
+//     //     <td><input type="text" id="description" placeholder="Enter an item"></td>
+//     //     <td><input type="number" id="price" value="0" placeholder=2000 required></td>
+//     //     <td><input type="number" id="quantity" value="0" placeholder=4 required></td>
+//     //     <td><input type="number" id="vat" value="0" placeholder=5 required></td>
+//     //     <td><input type="number" id="totals" disabled value=0></td>
+//     //     <td><i class="fa fa-ellipsis-h" style="font-size:24px"></i></td>
+//     // </tr>
+//     `;
+
+//     addTable.innerHTML +=template;
+// })
